192bdebd137bc9b1d270417fae071e7d664ede67
Marius <marius.chritin@gmail.com>
2022-11-23 09:27:34 +0100

    TEST PORT B13

--- SaveRobot/src/main.cpp
+++ SaveRobot/src/main.cpp
@@ -2,8 +2,10 @@
 
 void setup() {
   // put your setup code here, to run once:
+  DDRB = B01000000;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:zefzefzef
+  PORTB = 00000000;
 }
